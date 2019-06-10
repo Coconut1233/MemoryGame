f820b7910588789f73dfa45e368602aff78da821
Coconut1233 <spiridonov.michail1@gmail.com>
2019-06-10 12:43:49 +0200

    works

--- pexeso.py
+++ pexeso.py
@@ -1,68 +1,66 @@
-from PyQt5.QtWidgets import QApplication, QGridLayout, QPushButton, QWidget
-from PyQt5 import QtGui
-import random
 import os
-import time
+import random
+from functools import partial
+
+from PyQt5 import QtGui
+from PyQt5.QtCore import QEventLoop, QTimer
+from PyQt5.QtWidgets import QApplication, QGridLayout, QPushButton, QWidget
 
 ROWS = 4
 COLUMNS = 4
 PIC_PATH = "./pics"
 
 selected_tile = None
+in_timeout = False
 
-def tile_clicked(button):
-    idx = buttons.index(button)
-    x = 0
-    y = 0
-    while idx > 0:
-        if idx > 4:
-            x = x + 1
-            idx = idx - 4
-        else:
-            y = y + 1
-            idx = idx - 1
-    if selected_tile == None:
-        selected_tile = [pics[idx], x, y]
-        layout.addWidget(buttons[idx], x, y)
+def tile_clicked(idx, button, icon):
+    global selected_tile, in_timeout
+
+    print(idx)
+    x = idx // 4
+    y = idx % 4
+    print(x, y)
+    if in_timeout: return
+    button.setIcon(QtGui.QIcon(icon))
+
+    if selected_tile is None:
+        selected_tile = button
     else:
-        if selected_tile[0] == pics[idx]:
-            layout.addWidget(buttons[idx], x, y)
+        if selected_tile.icon != button.icon:
+            in_timeout = True
+            loop = QEventLoop()
+            QTimer.singleShot(500, loop.quit)
+            loop.exec_()
+            selected_tile.setIcon(QtGui.QIcon(None))
+            button.setIcon(QtGui.QIcon(None))
+            selected_tile = None
+            in_timeout = False
         else:
-            layout.addWidget(buttons[idx], x, y)
-            time.sleep(1)
-            layout.addWidget(buttons_hidden[idx], selected_tile[0], selected_tile[1])
-            layout.addWidget(buttons_hidden[idx], x, y)
+            button.setEnabled(False)
+            selected_tile.setEnabled(False)
+            selected_tile = None
 
- 
 app = QApplication([])
 window = QWidget()
-layout = QGridLayout();
+layout = QGridLayout()
 pics = []
 
 for pic in os.listdir(PIC_PATH):
     pics.append(os.path.join("./pics", pic))
 
-pics = pics*2
-print(pics)
+pics = pics * 2
 random.shuffle(pics)
 
 buttons = []
-buttons_hidden = []
-for icon in pics:
+for i, icon in enumerate(pics):
     ic = QtGui.QPixmap(os.path.realpath(str(icon)))
-    button = QPushButton()
-    hid_button = QPushButton()
-    button.setIcon(QtGui.QIcon(ic))
+    button = QPushButton(window)
+    x = i
+    button.icon = icon
+    button.clicked.connect(partial(tile_clicked, x, button, ic))
+    layout.addWidget(button, i // 4, i % 4)
     buttons.append(button)
-    buttons_hidden.append(hid_button)
 
-a = 0
-for x in range(0,ROWS):
-    for y in range(0,COLUMNS):
-        layout.addWidget(buttons_hidden[a], x, y)
-        a = a + 1
 window.setLayout(layout)
 window.show()
-tile_clicked(buttons[6])
 app.exec_()
-
